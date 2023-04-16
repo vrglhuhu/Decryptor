5c5c281d598fe026d8b75d94ecc07c9dfe516399
vrglhuhu <cheanbvergel@gmail.com>
2023-04-16 13:09:11 +0800

    The Psuedocode of my program

--- decryption.py
+++ decryption.py
@@ -2,3 +2,10 @@
 # Assignment No. 3
 # Questions No. 2
 
+# Create a header.
+# Ask for the name of the user.
+# Make a greeting message for the user.
+# Ask if the user wants to continue.
+# If user wants to continue, ask the user to input an encrypted text.
+# If user don't want to continue, print thank you.
+# Create a footer.
