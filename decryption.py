d17120bee49a7fb6fb27ca5960e5bb14340a549f
vrglhuhu <cheanbvergel@gmail.com>
2023-04-16 13:18:56 +0800

    Adding pyfiglet for header.

--- decryption.py
+++ decryption.py
@@ -3,9 +3,14 @@
 # Questions No. 2
 
 # Create a header.
+import pyfiglet
 print("")
 print("=" * 80)
 print("")
+welcome = pyfiglet.figlet_format("Welcome to my space".center(57, " "), font = "digital" )
+print(welcome)
+print("")
+print("=" * 80)
 print("Hi, I am Chean Bernard V. Vergel a first year college student at Polytechnic University of the Philippines.")
 print("")
 # Ask for the name of the user.
